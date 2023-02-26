680bd4ddc96627c80561eafe315c6730d32b266a
ControlGama <49002290+ControlGama@users.noreply.github.com>
2023-02-26 17:59:29 -0600

    Add botones de escalas

--- js/main.js
+++ js/main.js
@@ -1,52 +1,126 @@
 // 1 = tono 
 // 0.5 = Semitono
 
-const escalaMayorIntervalos = [1,1,0.5,1,1,1,0.5] 
+/*
+Escala:
+    1 - C
+    2 - C#
+    3 - D
+    4 - D#
+    5 - E
+    6 - F
+    7 - F#
+    8 - G
+    9 - G#
+    10 - A
+    11 - A#
+    12 - B
+ */
 
-function Mayor() {
-    console.log("dscs");
+//--Elementos WEB---
+const display = document.querySelector('.display');
+const keys = document.querySelectorAll('.key');
+
+const btnEscalaMayor = document.querySelector('#btnEscalaMayor');
+const btnEscalaMenor = document.querySelector('#btnEscalaMenor');
+const btnBack = document.querySelector('#btnBack');
+const btnNext = document.querySelector('#btnNext');
+
+//-- Parametros Iniciales --
+const indexNotas = getIndexNotas('G');
+let intervalos = [];
+let gv_escala = 0;
+
+
+btnEscalaMayor.addEventListener("click", (event) => {
+    const escalaMayorIntervalos = [1, 1, 0.5, 1, 1, 1, 0.5];
+    display.textContent = "C";
+    intervalos = escalaMayorIntervalos;
+
+    gv_escala = 3.5;
+
+    removeOnKeyClass();
+    printScale(gv_escala);
+});
+
+btnEscalaMenor.addEventListener("click", (event) => {
+    const escalaMenorIntervalos = [1, 0.5, 1, 1, 0.5, 1, 1];
+    display.textContent = "C";
+    intervalos = escalaMenorIntervalos;
+
+    gv_escala = 3.5; //3.5 = C
+
+    removeOnKeyClass();
+    printScale(gv_escala);
+});
+
+btnNext.addEventListener("click", (event) => {
+
+    display.textContent = "C#";
+    gv_escala = gv_escala + 0.5;
+
+    removeOnKeyClass();
+    printScale(gv_escala);
+});
+
+
+function printScale(p_escala) {
+
+    const OCTAVA = intervalos.reduce((a, b) => a + b, 0) + p_escala;
+
+    let keynum = 1;
+    let index = -1;
+    let nextKey = 0;
+    let lastKey = 0;
+
+    keys.forEach((item) => {
+        if ((keynum >= p_escala) && (keynum <= OCTAVA)) {
+
+            if (index != -1) {
+
+                nextKey = lastKey + intervalos[index];
+
+                if (keynum == nextKey) {
+                    lastKey = nextKey
+                    item.classList.add("onKey")
+                    index++;
+                }
+
+            } else {
+                item.classList.add("onKey")
+                lastKey = p_escala;
+                index = 0;
+            }
+        }
+        keynum += 0.5;
+    });
 }
 
-function Next() {
-    const keys = document.querySelectorAll('.key');
-
-    keys.forEach(element => {
-        element.classList.remove("onKey")
-        console.log(element);
+function removeOnKeyClass(){
+    keys.forEach((item) => {
+        item.classList.remove("onKey");
     });
+}
 
-    keys.forEach((element, index) => {
-
-        // const keyOn = escalaMayorIntervalos[index]  
-
-        switch (index) {
-            case 0:
-                element.classList.add("onKey")
-                break;
-            case 2:
-                element.classList.add("onKey")
-                break;
-            case 4:
-                element.classList.add("onKey")
-                break;
-            case 5:
-                element.classList.add("onKey")
-                break;
-            case 7:
-                element.classList.add("onKey")
-                break;
-            case 9:
-                element.classList.add("onKey")
-                break;
-            case 11:
-                element.classList.add("onKey")
-                break;
-            default:
-                break;
+function getIndexNotas(InitialKey) {
+    
+    const indexNotas = [];
+    const notas = ['A','A#','B','C','C#','D','D#','E','F','F#','G','G#']
+    let index = 1;
+    let notaIndex = notas.indexOf(InitialKey);
+    
+    keys.forEach((item) => {
+        
+        if (notaIndex >= notas.length){
+            notaIndex = 0;
         }
 
-        console.log(element);
-    });
+        const indexNota = [index, notas[notaIndex]];
+        indexNotas.push(indexNota);
+        notaIndex++;
+        index+=0.5;
+    });    
 
-
+    return indexNotas;
 }
+
